38b7035fa38e69283d9d891b92065877e65fccce
TrozieVN <hai682007@gmail.com>
2026-07-09 21:47:28 +0700

    create rooms table

--- database/createTable.js
+++ database/createTable.js
@@ -13,13 +13,45 @@ function createTable(callback) {
         (err) => {
 
             if (err) {
-                console.log("Lỗi tạo bảng:", err.message);
+                console.log("Lỗi tạo bảng users:", err.message);
                 return;
             }
 
             console.log("Đã tạo bảng users");
 
-            if (callback) callback();
+
+            db.run(
+                `
+                CREATE TABLE IF NOT EXISTS rooms (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT,
+                    province TEXT,
+                    district TEXT,
+                    ward TEXT,
+                    address TEXT,
+                    type TEXT,
+                    price TEXT,
+                    area TEXT,
+                    status TEXT,
+                    photo TEXT,
+                    description TEXT,
+                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+                )
+                `,
+                (err) => {
+
+                    if (err) {
+                        console.log("Lỗi tạo bảng rooms:", err.message);
+                        return;
+                    }
+
+                    console.log("Đã tạo bảng rooms");
+
+                    if (callback) callback();
+
+                }
+            );
+
         }
     );
 }
